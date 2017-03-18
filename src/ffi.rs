da3b2314eb02894158b899cc12a859e764e1b9ec
Linus Färnstrand <faern@faern.net>
2017-03-19 00:37:56 +0100

    Add ioctl-sys and define initial calls

--- src/ffi.rs
+++ src/ffi.rs
@@ -2,3 +2,7 @@
 pub mod pfvar {
     include!(concat!(env!("OUT_DIR"), "/pfvar.rs"));
 }
+
+ioctl!(none pf_start with b'D', 1);
+ioctl!(none pf_stop with b'D', 2);
+ioctl!(readwrite pf_get_status with b'D', 21; pfvar::pf_status);
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
+#[macro_use]
+extern crate ioctl_sys;
+
 pub mod ffi;
