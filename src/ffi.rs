e3bea8a1b5113ac57802bd88c50380f3683eced2
Linus Färnstrand <faern@faern.net>
2017-03-22 11:23:48 +0100

    Add comments to explain ioctl ffi calls

--- src/ffi.rs
+++ src/ffi.rs
@@ -3,6 +3,10 @@ pub mod pfvar {
     include!(concat!(env!("OUT_DIR"), "/pfvar.rs"));
 }
 
+// The definitions of the ioctl calls come from pfvar.h. Look for the comment "ioctl operations"
+// DIOCSTART
 ioctl!(none pf_start with b'D', 1);
+// DIOCSTOP
 ioctl!(none pf_stop with b'D', 2);
+// DIOCGETSTATUS
 ioctl!(readwrite pf_get_status with b'D', 21; pfvar::pf_status);
