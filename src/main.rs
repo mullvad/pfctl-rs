bd4398caeaf2e8b80d238518062b065038b1b15a
Linus Färnstrand <faern@faern.net>
2017-04-07 10:17:06 +0200

    Remove hiding how to use rule builder

--- src/main.rs
+++ src/main.rs
@@ -61,29 +61,34 @@ fn run() -> Result<()> {
         err => err.chain_err(|| "Unable to add redirect anchor")?,
     }
 
-    let pass_all_rule = rule_builder().build().unwrap();
+    let pass_all_rule =
+        pfctl::FilterRuleBuilder::default().action(pfctl::RuleAction::Pass).build().unwrap();
     pf.add_rule(anchor_name, &pass_all_rule).chain_err(|| "Unable to add rule")?;
 
     let from_net = IpNetwork::from_str("192.168.99.11/24").unwrap();
-    let from_net_rule = rule_builder().from(pfctl::Ip::from(from_net)).build().unwrap();
+    let from_net_rule = pfctl::FilterRuleBuilder::default()
+        .action(pfctl::RuleAction::Pass)
+        .af(pfctl::AddrFamily::Ipv4)
+        .from(pfctl::Ip::from(from_net))
+        .build()
+        .unwrap();
     pf.add_rule(anchor_name, &from_net_rule).chain_err(|| "Unable to add second rule")?;
 
-    let to_port_rule = rule_builder().to(pfctl::Port::from(9876)).build().unwrap();
+    let to_port_rule = pfctl::FilterRuleBuilder::default()
+        .action(pfctl::RuleAction::Pass)
+        .to(pfctl::Port::from(9876))
+        .build()
+        .unwrap();
     pf.add_rule(anchor_name, &to_port_rule).chain_err(|| "Unable to add third rule")?;
 
     let ipv6 = Ipv6Addr::new(0xbeef, 8, 7, 6, 5, 4, 3, 2);
-    let from_ipv6_rule = rule_builder()
-        .from(ipv6)
+    let from_ipv6_rule = pfctl::FilterRuleBuilder::default()
+        .action(pfctl::RuleAction::Pass)
         .af(pfctl::AddrFamily::Ipv6)
+        .from(ipv6)
         .build()
         .unwrap();
     pf.add_rule(anchor_name, &from_ipv6_rule).chain_err(|| "Unable to add fourth rule")?;
 
     Ok(())
-}
-
-fn rule_builder() -> pfctl::FilterRuleBuilder {
-    let mut rule_builder = pfctl::FilterRuleBuilder::default();
-    rule_builder.action(pfctl::RuleAction::Pass).af(pfctl::AddrFamily::Ipv4);
-    rule_builder
 }
