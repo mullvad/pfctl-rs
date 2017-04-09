b2d6190972c440c0230bc72bead0944df554288f
Linus Färnstrand <faern@faern.net>
2017-04-09 15:50:15 +0200

    Adapt test main to new features

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate ipnetwork;
 
 use ipnetwork::IpNetwork;
 
-use std::net::Ipv6Addr;
+use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
 
 mod errors {
@@ -43,32 +43,41 @@ fn run() -> Result<()> {
 
     let pass_all_rule =
         pfctl::FilterRuleBuilder::default().action(pfctl::RuleAction::Pass).build().unwrap();
+    let pass_all4_rule = pfctl::FilterRuleBuilder::default()
+        .action(pfctl::RuleAction::Pass)
+        .af(pfctl::AddrFamily::Ipv4)
+        .build()
+        .unwrap();
+    let pass_all6_rule = pfctl::FilterRuleBuilder::default()
+        .action(pfctl::RuleAction::Pass)
+        .af(pfctl::AddrFamily::Ipv6)
+        .build()
+        .unwrap();
     pf.add_rule(anchor_name, &pass_all_rule).chain_err(|| "Unable to add rule")?;
+    pf.add_rule(anchor_name, &pass_all4_rule).chain_err(|| "Unable to add rule")?;
+    pf.add_rule(anchor_name, &pass_all6_rule).chain_err(|| "Unable to add rule")?;
 
     let from_net = IpNetwork::from_str("192.168.99.11/24").unwrap();
     let from_net_rule = pfctl::FilterRuleBuilder::default()
         .action(pfctl::RuleAction::Pass)
-        .af(pfctl::AddrFamily::Ipv4)
         .from(pfctl::Ip::from(from_net))
         .build()
         .unwrap();
-    pf.add_rule(anchor_name, &from_net_rule).chain_err(|| "Unable to add second rule")?;
+    pf.add_rule(anchor_name, &from_net_rule).chain_err(|| "Unable to add IPv4 net rule")?;
 
     let to_port_rule = pfctl::FilterRuleBuilder::default()
         .action(pfctl::RuleAction::Pass)
         .to(pfctl::Port::from(9876))
         .build()
         .unwrap();
-    pf.add_rule(anchor_name, &to_port_rule).chain_err(|| "Unable to add third rule")?;
+    pf.add_rule(anchor_name, &to_port_rule).chain_err(|| "Unable to add port rule")?;
 
     let ipv6 = Ipv6Addr::new(0xbeef, 8, 7, 6, 5, 4, 3, 2);
     let from_ipv6_rule = pfctl::FilterRuleBuilder::default()
         .action(pfctl::RuleAction::Pass)
-        .af(pfctl::AddrFamily::Ipv6)
         .from(ipv6)
         .build()
         .unwrap();
-    pf.add_rule(anchor_name, &from_ipv6_rule).chain_err(|| "Unable to add fourth rule")?;
-
+    pf.add_rule(anchor_name, &from_ipv6_rule).chain_err(|| "Unable to add IPv6 rule")?;
     Ok(())
 }
