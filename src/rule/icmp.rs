16ac4ff08aee3bc7806265aa28215cf1e28fb9d6
Matt Schulte <mschulte@cloudflare.com>
2021-10-27 17:08:46 -0700

    Fix raw_code for `Timex` and add unit tests

--- src/rule/icmp.rs
+++ src/rule/icmp.rs
@@ -89,6 +89,7 @@ impl IcmpType {
         use IcmpType::*;
         match self {
             Unreach(unreach_code) => Some(*unreach_code as u8),
+            Timex(timex_code) => Some(*timex_code as u8),
             _ => None,
         }
     }
@@ -103,5 +104,91 @@ impl crate::conversion::CopyTo<crate::ffi::pfvar::pf_rule> for IcmpType {
             Some(raw_code) => raw_code + 1,
             None => 0,
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn all_codes_are_expected() {
+        // Expected values take from codes defined here:
+        // https://man.openbsd.org/icmp.4
+
+        assert_eq!(IcmpType::EchoRep.raw_code(), None);
+
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::NetUnreach).raw_code(),
+            Some(0)
+        );
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::HostUnreach).raw_code(),
+            Some(1)
+        );
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::ProtoUnreach).raw_code(),
+            Some(2)
+        );
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::PortUnreach).raw_code(),
+            Some(3)
+        );
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::NeedFrag).raw_code(),
+            Some(4)
+        );
+
+        assert_eq!(IcmpType::EchoReq.raw_code(), None);
+
+        assert_eq!(IcmpType::Timex(IcmpTimexCode::Transit).raw_code(), Some(0));
+        assert_eq!(
+            IcmpType::Timex(IcmpTimexCode::Reassembly).raw_code(),
+            Some(1)
+        );
+
+        assert_eq!(IcmpType::Trace.raw_code(), None);
+
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::RouterSol).raw_code(), None);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::RouterAdv).raw_code(), None);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::NeighbrSol).raw_code(), None);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::NeighbrAdv).raw_code(), None);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::Redir).raw_code(), None);
+    }
+
+    #[test]
+    fn all_types_are_expected() {
+        // Expected values take from codes defined here:
+        // https://man.openbsd.org/icmp.4
+
+        assert_eq!(IcmpType::EchoRep.raw_type(), 0);
+
+        assert_eq!(IcmpType::Unreach(IcmpUnreachCode::NetUnreach).raw_type(), 3);
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::HostUnreach).raw_type(),
+            3
+        );
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::ProtoUnreach).raw_type(),
+            3
+        );
+        assert_eq!(
+            IcmpType::Unreach(IcmpUnreachCode::PortUnreach).raw_type(),
+            3
+        );
+        assert_eq!(IcmpType::Unreach(IcmpUnreachCode::NeedFrag).raw_type(), 3);
+
+        assert_eq!(IcmpType::EchoReq.raw_type(), 8);
+
+        assert_eq!(IcmpType::Timex(IcmpTimexCode::Transit).raw_type(), 11);
+        assert_eq!(IcmpType::Timex(IcmpTimexCode::Reassembly).raw_type(), 11);
+
+        assert_eq!(IcmpType::Trace.raw_type(), 30);
+
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::RouterSol).raw_type(), 133);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::RouterAdv).raw_type(), 134);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::NeighbrSol).raw_type(), 135);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::NeighbrAdv).raw_type(), 136);
+        assert_eq!(IcmpType::Icmp6(Icmp6Type::Redir).raw_type(), 137);
     }
 }
